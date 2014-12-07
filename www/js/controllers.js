0f09c18eca0b16781b2f25e22ee9cc6682819284
ciekawy <ciekawy@gmail.com>
2014-12-07 14:38:09 +0100

    tak picture

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,26 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $cordovaCamera) {
+  $scope.takePicture = function() {
+    var options = { 
+        quality : 75, 
+        destinationType : Camera.DestinationType.FILE_URI, 
+        sourceType : Camera.PictureSourceType.CAMERA, 
+        allowEdit : true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageURI) {
+        $scope.imgURI = imageURI;
+    }, function(err) {
+        $scope.imageError = err;
+        // An error occured. Show a message to the user
+    });
+  };
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
